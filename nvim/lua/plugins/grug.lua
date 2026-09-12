fbf674e0c798d2664e9b39dfc0437628ce967473
harvest7777 <333harvest.moon333@gmail.com>
2026-09-11 21:22:13 -0700

    grug

--- nvim/lua/plugins/grug.lua
+++ nvim/lua/plugins/grug.lua
@@ -2,7 +2,9 @@ return
 {
   'MagicDuck/grug-far.nvim',
   config = function()
-    require('grug-far').setup({})
+    require('grug-far').setup({
+      windowCreationCommand = 'enew | lua vim.bo.buflisted = false',
+    })
     vim.keymap.set('n', '<leader>gn', function()
       local grug_far = require('grug-far')
       if grug_far.has_instance('main') then
