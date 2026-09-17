bf14b7cb5999188a895d30d08bfd764d575b6544
harvest7777 <333harvest.moon333@gmail.com>
2026-09-17 14:24:08 -0700

    grug fix

--- nvim/lua/plugins/grug.lua
+++ nvim/lua/plugins/grug.lua
@@ -3,7 +3,29 @@ return
   'MagicDuck/grug-far.nvim',
   config = function()
     require('grug-far').setup({
-      windowCreationCommand = 'enew | lua vim.bo.buflisted = false',
+      windowCreationCommand = 'enew | lua vim.bo.buflisted = false; vim.b.__grug_far_buffer = true',
+      -- Open result files in the window Grug replaced instead of creating a split.
+      -- Grug still reuses an already-loaded buffer when one exists.
+      openTargetWindow = {
+        preferredLocation = 'prev',
+      },
+    })
+
+    -- A new file opened with Goto reuses Grug's buffer, so make it visible
+    -- in bufferline once it becomes a normal file buffer.
+    vim.api.nvim_create_autocmd('BufFilePost', {
+      callback = function(args)
+        if not vim.b[args.buf].__grug_far_buffer then
+          return
+        end
+
+        vim.schedule(function()
+          if vim.api.nvim_buf_is_valid(args.buf) and vim.bo[args.buf].buftype == '' then
+            vim.bo[args.buf].buflisted = true
+            vim.b[args.buf].__grug_far_buffer = nil
+          end
+        end)
+      end,
     })
     vim.keymap.set('n', '<leader>gn', function()
       local grug_far = require('grug-far')
